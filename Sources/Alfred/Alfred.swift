3610d3580de73fc26774c757a248171a594483cc
Erik Opinaldo <erikopinaldo@gmail.com>
2024-04-27 21:24:51 -0400

    use ternary for conditional declaration of appBundlePath

--- Sources/Alfred/Alfred.swift
+++ Sources/Alfred/Alfred.swift
@@ -4,12 +4,7 @@ public class Alfred {
   private static let fs: FileManager = FileManager.default
   private static let home: URL = fs.homeDirectoryForCurrentUser
 
-  static let appBundlePath: URL
-  if fs.fileExists(atPath: "/Applications/Alfred 5.app") {
-    appBundlePath = URL(fileURLWithPath: "/Applications/Alfred 5.app")
-  } else {
-    appBundlePath = URL(fileURLWithPath: "/Applications/Alfred 4.app")
-  }
+  static let appBundlePath: URL = fs.fileExists(atPath: "/Applications/Alfred 5.app") ? URL(fileURLWithPath: "/Applications/Alfred 5.app") : URL(fileURLWithPath: "/Applications/Alfred 4.app")
 
   private static let alfredPlist: Plist =
     Plist(path: appBundlePath/"Contents"/"Info.plist")
